d12eee7fdf02d93ce6cad1085d99243b05113165
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2023-01-22 17:45:33 -0300

    first commit

---
+++ src/test/java/br/com/fiap/testespring/TesteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.testespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
